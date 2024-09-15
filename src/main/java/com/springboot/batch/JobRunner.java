d9a979a44952d68ed7f13eb87b93f163a16ad9a4
kkmjj <alswns1201@gmail.com>
2024-09-15 16:17:12 +0900

    배치 도메인 이해하기 . BATCH_JOB_EXECUTION_PARMAMS 만들어서 DB 확인해보기 - jobrunner 구성.

---
+++ src/main/java/com/springboot/batch/JobRunner.java
@@ -0,0 +1,33 @@
+package com.springboot.batch;
+
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JobRunner implements ApplicationRunner {
+
+    @Autowired//
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
+    //boot에서 제공하는 ApplicationRunner의 run을 사용.
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+      JobParameters jobParameters
+              =  new JobParametersBuilder()
+                .addString("name","user1")
+                        .toJobParameters();
+
+        jobLauncher.run(job,jobParameters);
+    }
+}
